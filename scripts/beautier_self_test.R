cbbdce8f057c1021e8426fe8b20bb3485d460a0e
richelbilderbeek <richel@richelbilderbeek.nl>
2019-09-07 12:54:55 +0200

    Run proper self-test

--- scripts/beautier_self_test.R
+++ scripts/beautier_self_test.R
@@ -1,3 +1,19 @@
 # beautier self-testing script
 
-print("Hello")
+library(beautier)
+
+# Get an example FASTA file
+input_filename <- get_fasta_filename()
+
+# The file created by beautier, a BEAST2 input file
+output_filename <- tempfile()
+
+# Use the default BEAUti settings to create a BEAST2 input file
+create_beast2_input_file_from_model(
+  input_filename,
+  output_filename,
+  inference_model = create_inference_model()
+)
+
+library(testthat)
+expect_true(file.exists(output_filename))
