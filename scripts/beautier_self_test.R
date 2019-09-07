9dce3a97fb5bc304f0d57feb5f052f065fb99f41
richelbilderbeek <richel@richelbilderbeek.nl>
2019-09-07 13:57:43 +0200

    Use another explicit namespace

--- scripts/beautier_self_test.R
+++ scripts/beautier_self_test.R
@@ -1,12 +1,26 @@
 # beautier self-testing script
 
+print("=====================")
+print("Self-testing beautier")
+print("=====================")
+
 library(beautier)
 
-print("Self-testing beautier")
+print("------------")
+print("Session info")
+print("------------")
 
 print(sessionInfo())
 
+print("--------------------------")
+print("beautier's package version")
+print("--------------------------")
+
 print(packageVersion("beautier"))
+
+print("---------------")
+print("Start self-test")
+print("---------------")
 
 # Get an example FASTA file
 input_filename <- get_fasta_filename()
@@ -26,7 +40,7 @@ library(testthat)
 expect_true(file.exists(output_filename))
 
 # Use the default BEAUti settings to create a BEAST2 input
-create_beast2_input_from_model(
+beautier::create_beast2_input_from_model(
   input_filename,
   output_filename,
   inference_model = create_inference_model()
